a3e600879218ca150b7c152ac260dd97ebb8143d
Denis Cousineau <Denis.Cousineau@uottawa.ca>
2021-07-20 21:20:52 +0200

    damn hyperlink...

--- R/superbShiny.R
+++ R/superbShiny.R
@@ -4,14 +4,16 @@
 #' @md
 #'
 #' @description The function ``suberbShiny()`` provides a simple user interface 
-#'      to plot standard error or confidence interval for various descriptive 
-#'      statistics under various designs, population size and purposes,
-#'      according to the ``suberb`` framework. See \insertCite{cgh21}{superb} for more.
-#'      Also see this \insertCite{w21}{youtube} presentation for a demo using
-#'      the shinyapps.io installation from https://dcousin3.shinyapps.io/superbshiny/
-#'      Limitations: it is not possible to use custom-made statistics with the 
-#'      graphical user interface, nor is it possible to request an adjustment for cluster-
-#'      randomized sampling. These options are available with ``superbPlot()``.
+#'   to plot standard error or confidence interval for various descriptive 
+#'   statistics under various designs, population size and purposes,
+#'   according to the ``suberb`` framework. See \insertCite{cgh21}{superb} for more.
+#'   Also see this \href{https://www.youtube.com/watch?v=rw_6ll5nVus/}{video} 
+#'   from \insertCite{w21}{superb} for a demo using
+#'   the shinyapps.io installation accessible at 
+#'   \href{https://dcousin3.shinyapps.io/superbshiny/}{dcousin3.shinyapps.io/superbshiny}
+#'   Limitations: it is not possible to use custom-made statistics with the 
+#'   graphical user interface, nor is it possible to request an adjustment for cluster-
+#'   randomized sampling. These options are available with ``superbPlot()``.
 #'
 #' @return A plot that can be cut-and-paste.
 #'
